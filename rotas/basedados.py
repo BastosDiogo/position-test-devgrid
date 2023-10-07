90166670b8b1742ab8ab1f1572a69cf8c836891a
BastosDiogo <bmnetto.diogo@gmail.com>
2023-10-07 12:51:30 -0300

    Endpoint resetar e descricoes.

--- rotas/basedados.py
+++ rotas/basedados.py
@@ -2,13 +2,18 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from servicos.usuarios import Usuarios
 from servicos.clima_cidades import ClimaCidades
+from utilidades.descricoes_endpoint import Descricoes
 
 router = APIRouter(prefix="/base-dados", tags=["Gestao Base dados"])
 
 usuarios = Usuarios()
 dados_clima = ClimaCidades()
+descricoes = Descricoes()
 
-@router.delete("/limpar-base-dados")
+@router.delete(
+    "/limpar-base-dados",
+    description=descricoes.deletar_base_dados()
+)
 async def limpar_base_dados(usuario_id:str):
     usurio_na_base = usuarios.buscar_usuario(usuario_id)
     if len(usurio_na_base) == 0:
@@ -23,4 +28,26 @@ async def limpar_base_dados(usuario_id:str):
     return JSONResponse(
             status_code=200,
             content={"mensagem": "Dados apagados com sucesso."}
+    )
+
+
+@router.delete(
+    "/resetar-cenario",
+    description=descricoes.resetar_cenario()
+)
+async def resetar_cenario_do_aplicativo(usuario_id:str):
+    usurio_na_base = usuarios.buscar_usuario(usuario_id)
+    if len(usurio_na_base) == 0:
+        return JSONResponse(
+            status_code=404,
+            content={
+                "ERRO": f"Usuário {usuario_id} NÃO ENCONTRADO.",
+                "mensagem": "Verifique se você usou o endpoit para gerar usuários."
+            }
+        )
+    dados_clima.limpar_base_clima
+    usuarios.limpar_base_usuarios
+    return JSONResponse(
+            status_code=200,
+            content={"mensagem": "Base de usuarios e de cidades, apagadas com sucesso."}
     )
--- rotas/clima.py
+++ rotas/clima.py
@@ -4,13 +4,15 @@ from servicos.usuarios import Usuarios
 from servicos.clima_cidades import ClimaCidades
 from fastapi import APIRouter, BackgroundTasks
 from servicos.armazenamento import Armazenar
+from utilidades.descricoes_endpoint import Descricoes
 
 router = APIRouter(prefix="/clima", tags=["Endpoints API clima"])
 
 usuarios = Usuarios()
 dados_clima = ClimaCidades()
+descricoes = Descricoes()
 
-@router.get("/gerar-usuario-id")
+@router.get("/gerar-usuario-id", description=descricoes.gerar_usuario())
 async def gerar_usuario_id():
     usuario_criado = usuarios.criar_usuario()
     if len(usuario_criado) == 0:
@@ -29,7 +31,10 @@ async def gerar_usuario_id():
     )
 
 
-@router.post("/armazenar-temperaturas")
+@router.post(
+    "/armazenar-temperaturas",
+    description=descricoes.armazenar_temperatura()
+)
 async def armazenar_temperaturas_da_lista_de_cidades(
     usuario_id:str,
     background: BackgroundTasks
@@ -52,7 +57,10 @@ async def armazenar_temperaturas_da_lista_de_cidades(
     )
 
 
-@router.get("/percentual-armazenado")
+@router.get(
+    "/percentual-armazenado",
+    description=descricoes.percentual_armazenado()
+)
 async def conferir_percentual_dos_dados_armazenado(usuario_id:str):
     usurio_na_base = usuarios.buscar_usuario(usuario_id)
     if len(usurio_na_base) == 0:
@@ -67,7 +75,10 @@ async def conferir_percentual_dos_dados_armazenado(usuario_id:str):
     return JSONResponse(status_code=200, content=percentual)
 
 
-@router.get("/trazer-todos-dados-climaticos-armazenados")
+@router.get(
+    "/trazer-todos-dados-climaticos-armazenados",
+    description=descricoes.todos_dados_climaticos()
+)
 async def trazer_todos_dados_climaticos_armazenados(usuario_id:str):
     usurio_na_base = usuarios.buscar_usuario(usuario_id)
     if len(usurio_na_base) == 0:
@@ -87,7 +98,10 @@ async def trazer_todos_dados_climaticos_armazenados(usuario_id:str):
     )
 
 
-@router.get("/trazer-dados-climaticos-armazenados-cidade/{cidade_id}")
+@router.get(
+    "/trazer-dados-climaticos-armazenados-cidade/{cidade_id}",
+    description=descricoes.dados_climaticos_de_uma_cidade()
+)
 async def trazer_todos_dados_climaticos_armazenados(
     usuario_id:str,
     cidade_id:str
